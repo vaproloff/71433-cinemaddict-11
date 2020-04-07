17049bb7524c35df65baa7aa395aed32c99d8749
Konstantin Pivnov <vaproloff@ya.ru>
2020-04-08 01:39:34 +0300

    Уровень пользователя, допоказ по кнопке Loadmore количество фильмов в фильтрах

--- src/main.js
+++ src/main.js
@@ -8,40 +8,51 @@ import {renderElement} from "./utils";
 import {generateFilmBase} from "./mocks/film-cards";
 // import {createFilmPopup} from "./components/film-popup";
 
-const FILMS_COUNT = 30;
+const FILMS_COUNT = Math.round(Math.random() * 5) + 15;
 const FILMS_TO_RENDER = 5;
 const FILMS_EXTRA_TO_RENDER = 2;
 
-// Рендер статуса пользователя
+const renderFilmsPack = (container, filmsPack) => {
+  filmsPack.forEach((it) => {
+    renderElement(container, createFilmCard(it)); // Рендер карточек фильмов
+  });
+};
+
+const films = generateFilmBase(FILMS_COUNT);
+let filmCardsRendered = Math.min(films.length, FILMS_TO_RENDER);
+
 const headerContainer = document.querySelector(`header.header`);
-renderElement(headerContainer, createUserLevel());
+renderElement(headerContainer, createUserLevel(films.filter((it) => it.isWatched).length)); // Рендер статуса пользователя
+renderElement(document.querySelector(`.footer__statistics`), `<p>${films.length}</p>`); // Рендер общего кол-ва фильмов
 
 const mainContainer = document.querySelector(`main.main`);
-renderElement(mainContainer, createMainControl()); // Рендер главного меню
+renderElement(mainContainer, createMainControl(films)); // Рендер главного меню
 renderElement(mainContainer, createSortingControl()); // Рендер панели сортировки
 renderElement(mainContainer, `<section class="films"></section>`); // Рендер секции с фильмами
 const filmsSection = mainContainer.querySelector(`section.films`);
 
-const films = generateFilmBase(FILMS_COUNT);
-
 // Рендер всех фильмов
 renderElement(filmsSection, createFilmsSection());
 const allFilmsListContainer = filmsSection.querySelector(`.films-list__container`);
-films.slice(0, FILMS_TO_RENDER).forEach((it) => {
-  renderElement(allFilmsListContainer, createFilmCard(it)); // Рендер карточек фильмов
-});
-renderElement(filmsSection.querySelector(`section.films-list`), createShowmoreButton()); // Рендер кнопки Loadmore
+renderFilmsPack(allFilmsListContainer, films.slice(0, FILMS_TO_RENDER));
+if (films.length > filmCardsRendered) {
+  renderElement(filmsSection.querySelector(`section.films-list`), createShowmoreButton()); // Рендер кнопки Loadmore
+  const loadMoreButton = filmsSection.querySelector(`.films-list__show-more`);
+  loadMoreButton.addEventListener(`click`, () => {
+    renderFilmsPack(allFilmsListContainer, films.slice(filmCardsRendered, filmCardsRendered + FILMS_TO_RENDER));
+    filmCardsRendered = (filmCardsRendered + FILMS_TO_RENDER) > films.length ? films.length : (filmCardsRendered + FILMS_TO_RENDER);
+    if (filmCardsRendered === films.length) {
+      loadMoreButton.classList.add(`visually-hidden`);
+    }
+  });
+}
 
-// Рендер Top rated фильмов
+// Рендер Extra фильмов
 renderElement(filmsSection, createFilmsSection(`Top rated`));
 renderElement(filmsSection, createFilmsSection(`Most commented`));
 const topRatedFilmsListContainer = filmsSection.querySelectorAll(`.films-list--extra .films-list__container`)[0];
-films.slice(0).sort((a, b) => b.rating - a.rating).slice(0, FILMS_EXTRA_TO_RENDER).forEach((it) => {
-  renderElement(topRatedFilmsListContainer, createFilmCard(it)); // Рендер карточек фильмов
-});
+renderFilmsPack(topRatedFilmsListContainer, films.slice(0).sort((a, b) => b.rating - a.rating).slice(0, FILMS_EXTRA_TO_RENDER));
 const mostCommentedFilmsListContainer = filmsSection.querySelectorAll(`.films-list--extra .films-list__container`)[1];
-films.slice(0).sort((a, b) => b.comments.length - a.comments.length).slice(0, FILMS_EXTRA_TO_RENDER).forEach((it) => {
-  renderElement(mostCommentedFilmsListContainer, createFilmCard(it)); // Рендер карточек фильмов
-});
+renderFilmsPack(mostCommentedFilmsListContainer, films.slice(0).sort((a, b) => b.comments.length - a.comments.length).slice(0, FILMS_EXTRA_TO_RENDER));
 
 // renderElement(document.querySelector(`body`), createFilmPopup(films[0]));
